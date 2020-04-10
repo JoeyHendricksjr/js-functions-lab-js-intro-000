7d7526522b2112f64f0848cc0adcee101d710c42
Joey Hendricks <joeysguitartabs@yahoo.com>
2020-04-10 23:39:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() {
   return 'Happy holidays!';
 }
 
-function happyHolidaysTo(aName) {
-  return `Happy holidays ${aName}!`;
+function happyHolidaysTo(name) {
+  return `Happy holidays ${name}!`;
 }
